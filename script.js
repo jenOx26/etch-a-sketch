8921ca518f4261c44e0692b17bd24b21e66b0194
Jen Ox <jnochsner26@gmail.com>
2022-05-19 15:20:24 -0600

    Add ability to change grid size

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-const grid = document.querySelector(".grid");
+let sizeBtn = document.querySelector('.sizeBtn');
+sizeBtn.addEventListener('click', changeSize);
 
-for (let i=1; i < 257; i++) {
-    let div = document.createElement("div");
-    grid.appendChild(div);
-    div.addEventListener('mouseover', function(event) {
-        event.target.style.backgroundColor = 'gray';
-    });
+function changeSize() {
+    let size = parseInt(prompt("What size grid would you like?"));
+    if (size <=100 && size >= 2) {
+        createGrid(size);
+    } else {
+        alert('Pick a size between 2 and 100!');
+    }
+}
+
+function createGrid(size) {
+    let grid = document.querySelector(".grid");
+    let cells = grid.querySelectorAll("div");
+    cells.forEach((div) => div.remove());
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let amount = size * size;
+    for (let i=0; i < amount; i++) {
+        let cell = document.createElement("div");
+        cell.style.backgroundColor = 'gray';
+        grid.insertAdjacentElement('beforeEnd', cell)
+        cell.addEventListener('mouseover', function(event) {
+            event.target.style.backgroundColor = 'black';
+        });
+    }
 }
