0828a14b80b885e546b19e511d62ffb0d6172605
Jen Ox <jnochsner26@gmail.com>
2022-05-19 11:30:11 -0600

    Add hover effect

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ const grid = document.querySelector(".grid");
 for (let i=1; i < 257; i++) {
     let div = document.createElement("div");
     grid.appendChild(div);
+    div.addEventListener('mouseover', function(event) {
+        event.target.style.backgroundColor = 'gray';
+    });
 }
